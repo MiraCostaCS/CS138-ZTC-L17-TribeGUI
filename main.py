d5aab0ed14b37010845b37babe47bde6f62fc3c6
kevinmlewis <kevinmlewis@gmail.com>
2026-08-15 15:05:22 -0700

    Initialize main.py with Tkinter setup
    Added initial structure and placeholders for a Tkinter application.

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+DEVELOPER: <your name>
+COLLABORATORS: <anyone you worked with>
+'''
+"""A one line summary of the program, terminated by a period.
+Leave one blank line. The rest of this docstring should contain an
+overall description of the program.
+"""
+##########################################
+# IMPORTS:
+##########################################
+from tkinter import *
+##########################################
+# CLASS DEFINITION:
+##########################################
+#<replace this line with your TribeRecord class from Lab 15>
+##########################################
+# GUI FUNCTIONS:
+##########################################
+#<replace this line with your button/entry handler functions>
+##########################################
+# MAIN PROGRAM:
+##########################################
+#<replace this line with your root = Tk() setup, widgets, and root.mainloop()>
